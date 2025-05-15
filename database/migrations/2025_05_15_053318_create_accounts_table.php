023ce2098f6dc57fb97c2eee127adacce0220f88
ArbSoftware <satabhishar@arbsoft.comm>
2025-05-15 14:33:53 +0530

    modified

--- database/migrations/2025_05_15_053318_create_accounts_table.php
+++ database/migrations/2025_05_15_053318_create_accounts_table.php
@@ -4,14 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAccountsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('accounts', function (Blueprint $table) {
+        Schema::create('Accounts', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->string('account_name');
@@ -32,6 +32,6 @@ class CreateAccountsTable extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('accounts');
+        Schema::dropIfExists('Accounts');
     }
 };
